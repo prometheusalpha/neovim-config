d237c5af1828ac114c20ba4142307187648aaf23
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2022-12-03 17:34:17 +0700

    feat(nvim-tree): add project.nvim support

--- lua/window_plugin.lua
+++ lua/window_plugin.lua
@@ -1,15 +1,21 @@
 require("util")
 
-map("n", "<Leader>b", ":NvimTreeToggle<cr>", {silent = true})
+map("n", "<Leader>b", ":NvimTreeToggle<cr>", { silent = true })
 require("nvim-tree").setup({
-  renderer = {
-    group_empty = true
-  }
+	sync_root_with_cwd = true,
+	respect_buf_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_root = true,
+	},
+	renderer = {
+		group_empty = true,
+	},
 })
 require("leap").add_default_mappings()
 require("auto-save").setup()
-require("nvim-treesitter.configs").setup {
-    highlight = { enable = true },
-    incremental_selection = { enable = true },
-    textobjects = { enable = true },
-}
+require("nvim-treesitter.configs").setup({
+	highlight = { enable = true },
+	incremental_selection = { enable = true },
+	textobjects = { enable = true },
+})
